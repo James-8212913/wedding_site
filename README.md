21f59a518e18ce4d38363299d3f8c1a80f4304ab
James Murray <james.w.murray@student.uts.edu.au>
2021-07-18 01:54:06 +1000

    minimal framework

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This is a website that was built for our wedding. It is also an example of a way that Python can be used to build and deploy a static website.
+
+# Create a virtual environment
+
+```{.bash}
+# create the environment - python 3.8 is the default for Netlify the time of writing
+$python3 -m venv venv
+ # activate the environment
+$source venv/bin/activate
+```
--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', title = 'Home')
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title = 'On The Day')
-
-@app.route("/other_stuff")
-def other_stuff():
-    return render_template('other_stuff.html', title = "Other Stuff")
+def index():
+    return render_template('index.html')
 
 if __name__ == '__name__':
     app.run(debug=True)
