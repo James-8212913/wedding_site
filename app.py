86b9fef572a7a5f3252fd6cf1255fb5b94d73cbe
James Murray <james.w.murray@student.uts.edu.au>
2021-07-18 04:32:52 +1000

    updated details and content v0.5

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route("/about/")
+def about():
+    return render_template('about.html')
+
+@app.route("/other_stuff/")
+def other_stuff():
+    return render_template('other_stuff.html')
+
 if __name__ == '__name__':
     app.run(debug=True)
